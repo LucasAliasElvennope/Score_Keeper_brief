a7b7882593bcf92639e3be66570a3eb9b2cd6cae
LucasAliasElvennope <lucasmoreaupro0@gmail.com>
2025-05-21 09:33:08 +0200

    feat : create constant to identify scoreTeam1 on js

--- js/score_keeper.js
+++ js/score_keeper.js
@@ -1,5 +1,7 @@
 const resetBtn = document.getElementById("resetBtn");
 const player1Btn = document.getElementById("player1Btn");
 const player2Btn = document.getElementById("player2Btn");
+const scoreTeam1 = document.getElementById("scoreTeam1");
+
 
 
