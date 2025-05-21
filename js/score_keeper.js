b0e2178ce1c3f15f16ed9b346c7aeede0f45f877
LucasAliasElvennope <lucasmoreaupro0@gmail.com>
2025-05-21 14:53:10 +0200

    feat : adding the use strict

--- js/score_keeper.js
+++ js/score_keeper.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const resetBtn = document.getElementById("resetBtn");
 const player1Btn = document.getElementById("player1Btn");
 const player2Btn = document.getElementById("player2Btn");
