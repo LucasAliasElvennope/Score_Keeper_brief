06bd7ff8ca41687412911e415c220423b98d8d70
LucasAliasElvennope <lucasmoreaupro0@gmail.com>
2025-05-21 09:46:43 +0200

    feat : changing two const name

--- js/score_keeper.js
+++ js/score_keeper.js
@@ -1,9 +1,10 @@
 const resetBtn = document.getElementById("resetBtn");
 const player1Btn = document.getElementById("player1Btn");
 const player2Btn = document.getElementById("player2Btn");
-const scoreTeam1 = document.getElementById("scoreTeam1");
-const scoreTeam2 = document.getElementById("scoreTeam2");
+const scoreTeam1Afficher = document.getElementById("scoreTeam1");
+const scoreTeam2Afficher = document.getElementById("scoreTeam2");
 const scoreMaxAfficher = document.getElementById("scoreMaxAfficher");
+
 let scoreMax = 5;
 let scoreTeam1 = 0;
 let scoreTeam2 = 0;
