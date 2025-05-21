cf86117756c9744d1b3a9e3b4bc89ad77b16ee11
LucasAliasElvennope <lucasmoreaupro0@gmail.com>
2025-05-21 09:35:16 +0200

    feat : create constant to identify scoreMaxAfficher  on js

--- js/score_keeper.js
+++ js/score_keeper.js
@@ -3,6 +3,8 @@ const player1Btn = document.getElementById("player1Btn");
 const player2Btn = document.getElementById("player2Btn");
 const scoreTeam1 = document.getElementById("scoreTeam1");
 const scoreTeam2 = document.getElementById("scoreTeam2");
+const scoreMaxAfficher = document.getElementById("scoreMaxAfficher");
+
 
 
 
